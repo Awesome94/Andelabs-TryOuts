447cce4eac9cceb30ba080a8ba4af000eeb6cf4e
Awesome <suubidavid@gmail.com>
2016-10-12 23:33:23 +0300

    new changes made

--- Factorial.py
+++ Factorial.py
@@ -1,5 +1,6 @@
-def factorial(number):
+def factorial(number):  # Created a Function called factorial with the number argument
+    # type: (object) -> object
     if number == 0:
-        return 1
-    else:
-        return number * factorial(number-1)
+        return 1  # if the number is equal to 0 the program should return one but if its more then it...
+    else:  # should return the factorial of that given number
+        return number * factorial(number - 1)
